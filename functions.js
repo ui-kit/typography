6f09abf2459cd533f9621916febb53382e36143f
Doc <michael.vanasse@optiv.com>
2016-03-09 19:26:18 -0600

    use root for variable/prop resolution

--- functions.js
+++ functions.js
@@ -1,51 +1,50 @@
-import vars from './variables';
+import Vars from './index.js';
 import {pascalcase} from 'stringcase';
 
-export default function scale(opts) {
-  var range = opts.range || vars.range;
-  var calcFn = calc(opts);
-  return toArray(range).map((_, i) => calcFn(i)).reverse();
-}
+// TODO clean this file up
 
-export function calc(opts) {
-  var along = opts.along || vars.along;
-  var mid = opts.mid || vars.mid;
-  var round = opts.round || vars.round;
-  var Scale = opts.scale || vars.scale;
-  var roundFn = round ? ((n) => Math.round(n * round) / round) : n => n;
+export default function(vars) {
 
-  return (i) => {
-    if (i === mid - 1) return roundFn(along);
-    if (i < mid) return roundFn(div(along, Scale, mid - i - 1));
-    return roundFn(mult(along, Scale, i - mid + 1))
+  return {scale, coerce, toHash, toArray};
+
+  function scale(opts) {
+    var range = opts.range || vars.range;
+    var calcFn = calc(opts);
+    return toArray(range).map((_, i) => calcFn(i)).reverse();
   }
+
+  function calc(opts) {
+    var along = opts.along || vars.along;
+    var mid = opts.mid || vars.mid;
+    var round = opts.round || vars.round;
+    var Scale = opts.scale || vars.scale;
+    var roundFn = round ? ((n) => Math.round(n * round) / round) : n => n;
+
+    return (i) => {
+      if (i === mid - 1) return roundFn(along);
+      if (i < mid) return roundFn(div(along, Scale, mid - i - 1));
+      return roundFn(mult(along, Scale, i - mid + 1))
+    }
+  }
+
 }
 
-export function toArray(range) {
+function toArray(range) {
   return (range || '').split(' ').filter(x => x);
 }
 
-export function toHash(list) {
+function toHash(list) {
   if (typeof list === 'string') list = list.split(' ');
   return list.reduce((a, n) => { a[n] = true; return a; }, {});
 }
 
-export function lookup(root, props) {
-  root = root || {};
-  props = props || {};
-  return Object.keys(vars).reduce((acc, n) => {
-    acc[n] = props[n] || root[`typography${pascalcase(n)}`] || vars[n];
-    return acc;
-  }, {});
-}
-
-export function coerce(val) {
+function coerce(val) {
   if (val === 'false' || val === 0 || val === '0') val = false;
   else if (val === 'true' || val === 1 || val === '1') val = true;
   return val;
 }
 
-export function partsFromRange(range, props) {
+function partsFromRange(range, props) {
   if (typeof range === 'string') range = toArray(range);
   if (props.at) props[props.at] = true;
   var len = range.length;
---
+++ index.js
@@ -0,0 +1,11 @@
+export default function(root, props) {
+  return root.get(module.id, props, {
+    scale: 1.125,
+    along: 1.5,
+    base: 12,
+    round: 10,
+    range: 'h1 h2 h3 h4 h5 h6 p small',
+    margins: 'h1 h2 h3 h4 h6',
+    mid: 2
+  });
+}
